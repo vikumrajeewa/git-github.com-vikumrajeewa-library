1c82743c8addb8d9b5b810f9c4d2b43f3abf3049
vikum <vikumrp@gmail.com>
2023-08-04 22:37:12 +0530

    SearchForm.vue

---
+++ online-library/src/components/SearchForm.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="search-form">
+      <input type="text" v-model="searchQuery" @input="searchBooks" placeholder="Search by title, description, or genre" />
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    data() {
+      return {
+        searchQuery: '',
+      };
+    },
+    methods: {
+      searchBooks() {
+        this.$emit('search', this.searchQuery);
+      },
+    },
+  };
+  </script>
+  
