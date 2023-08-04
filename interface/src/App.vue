d9e96706bc323e4035aca1a074b39c2952171ac2
vikum <vikumrp@gmail.com>
2023-08-04 20:27:05 +0530

    Use the components in App.vue

--- interface/src/App.vue
+++ interface/src/App.vue
@@ -4,82 +4,48 @@ import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
-    </div>
-  </header>
-
-  <RouterView />
+  <div>
+    <h1>Online Library</h1>
+    <SearchBar @search="handleSearch"></SearchBar>
+    <BookTable :books="filteredBooks"></BookTable>
+  </div>
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
-  max-height: 100vh;
-}
+<script>
+import SearchBar from "./components/SearchBar.vue";
+import BookTable from "./components/BookTable.vue";
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-nav {
-  width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
-}
-
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
-}
-</style>
+export default {
+  components: {
+    SearchBar,
+    BookTable,
+  },
+  data() {
+    return {
+      books: [
+        // Dummy data for demonstration, replace this with your actual data.
+        {
+          id: 1,
+          title: "Book 1",
+          description: "Description of Book 1",
+          genre: "Fiction",
+          price: 20.0,
+        },
+        // Add more books here...
+      ],
+      searchText: "",
+    };
+  },
+  computed: {
+    filteredBooks() {
+      // Filter the books based on the searchText.
+      return this.books.filter((book) => {
+        const search = this.searchText.toLowerCase();
+        return (
+          book.title.toLowerCase().includes(search) ||
+          book.description.toLowerCase().includes(search) ||
+          book.genre.toLowerCase().includes(search)
+        );
+      });
+    },
+  },
--- interface/src/main.js
+++ interface/src/main.js
@@ -4,8 +4,14 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 
-const app = createApp(App)
+// const app = createApp(App)
 
-app.use(router)
+// app.use(router)
 
-app.mount('#app')
+// app.mount('#app')
+
+
+
+// Create the Vue instance and mount the root component to the DOM
+createApp(App).use(router).mount('#app');
+
